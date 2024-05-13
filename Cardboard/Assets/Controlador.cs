3c2e314e75badcf20b4fbc8d67742a88301c7c76
ElErizio <theericyea100@hotmail.com>
2024-05-13 11:56:41 -0600

    Se agrega proyecto
    Se agrego el proyecto para terminarlo en casa en caso de no acabar en clase

---
+++ Cardboard/Assets/Controlador.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    public Material desactivado, activado;
+    Renderer renderers;
+    public void Start()
+    {
+        renderers = GetComponent<Renderer>();
+        SetMaterial(false);
+    }
+
+
+    public void BottonActivate()
+    {
+        SetMaterial(true);
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+
+    public void OnPointerEnter()
+    {
+        StartCoroutine(activador());
+    }
+
+    public void OnPointerExit()
+    {
+        SetMaterial(false);
+    }
+
+    IEnumerator activador()
+    {
+        yield return new WaitForSeconds(3);
+        BottonActivate();
+    }
+
+    private void SetMaterial(bool gazedAt)
+    {
+        if (desactivado != null && activado != null)
+        {
+            renderers.material = gazedAt ? activado : desactivado;
+        }
+    }
+}
